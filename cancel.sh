c106396f57865d4a36633bd81e68aafac9dd390b
terrycojones <terry@jon.es>
2018-08-08 01:48:03 +0100

    Updated cancel.sh to log the cancelation.

--- cancel.sh
+++ cancel.sh
@@ -7,6 +7,7 @@ then
     jobids=$(cat $JOBIDS)
     scancel $jobids
     rm $JOBIDS
+    echo Canceled jobs $jobids at $(date). >> $LOG
     echo Canceled jobs $jobids.
 else
     echo Nothing was running.
